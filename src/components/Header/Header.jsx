cb52eb643b385c956e07c6123e23b877dce8edd4
Nyaga25 <nicknyaga@icloud.com>
2022-03-16 01:59:48 +0300

    first commit

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { Autocomplete } from '@react-google-maps/api';
+import { AppBar, Toolbar, Typography, InputBase, Box } from '@material-ui/core';
+import SearchIcon from '@material-ui/icons/Search';
+ 
+
+const Header = () => {
+    return (
+      <AppBar position="static">
+          <Toolbar className={classes.toolbar}>
+              <Typography variant='h5' className={classes.title}>
+                  Travel Advisor
+              </Typography>
+              <Box display="flex">
+                 <Typography variant='h6' className={classes.title}>
+                  Explore new places
+              </Typography> 
+              <Autocomplete>
+                  <div className={classes.search}>
+                      <div className={classes.searchIcon}>
+                          <SearchIcon />
+
+                    </div>
+                    <InputBase placeholder="Search..." classes={{ root: classes.inputRoot, input: classes.inputInput}} />
+
+                 </div>
+              </Autocomplete>
+              </Box>
+
+          </Toolbar>
+      </AppBar>
+
+    );
+}
+export default Header;
