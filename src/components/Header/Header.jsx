bca05840e58e46a90987689cd1afd8549f1eca51
Nyaga25 <nicknyaga@icloud.com>
2022-03-20 14:20:49 +0300

    update

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,7 +4,9 @@ import { AppBar, Toolbar, Typography, InputBase, Box } from '@material-ui/core';
 import SearchIcon from '@material-ui/icons/Search';
  
 
+import useStyles from './styles';
 const Header = () => {
+    const classes = useStyles();
     return (
       <AppBar position="static">
           <Toolbar className={classes.toolbar}>
@@ -15,7 +17,7 @@ const Header = () => {
                  <Typography variant='h6' className={classes.title}>
                   Explore new places
               </Typography> 
-              <Autocomplete>
+              <>
                   <div className={classes.search}>
                       <div className={classes.searchIcon}>
                           <SearchIcon />
@@ -24,7 +26,7 @@ const Header = () => {
                     <InputBase placeholder="Search..." classes={{ root: classes.inputRoot, input: classes.inputInput}} />
 
                  </div>
-              </Autocomplete>
+              </>
               </Box>
 
           </Toolbar>
--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,8 +1,23 @@
 import React from 'react';
+import GoogleMapReact from 'google-map-react';
+import { Paper, Typography, useMediaQuery } from '@material-ui/core';
+import LocationOutlinedIcon from '@material-ui/icons/LocationOutlined';
+import Rating from '@material-ui/lab';
 
+import useStyles from './style';
 const Map = () => {
+    const classes = useStyles();
+    const isMobile = useMediaQuery('(min-width:600px)');
     return (
-        <h1>Map</h1>
+        <div className={classes.mapContainer}>
+            <GoogleMapReact
+            >
+
+            </GoogleMapReact>
+
+
+        </div>
+      
 
     );
 }
